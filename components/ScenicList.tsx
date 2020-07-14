957b622b6820ff4cea386dc74aaea710ea2586cf
LiuYonghui <liuyonghui@jetair.com.cn>
2020-07-14 22:52:06 +0800

    feat: 修改作品名字

--- components/ScenicList.tsx
+++ components/ScenicList.tsx
@@ -2,23 +2,51 @@ import React from 'react';
 import { Card } from 'antd';
 
 const ScenicList = () => {
-  const data = [{
-    
-  }];
+  const data = [
+    {
+      /** 作品名称 */
+      name: '印度南迪亚尔，巴拉吉神庙 / Sameep Padora & Associates',
+      /** 作品链接 */
+      href: '/work/expmale',
+      /** 作品类型 */
+      type: {
+        text: '景观',
+        href: '/work/expmale'
+      },
+      /** 作品分类 */
+      category: {
+        text: '纪念性空间',
+        href: '/work/expmale'
+      },
+      /** 作品地区 */
+      region: {
+        text: '墨西哥',
+        href: '/work/expmale'
+      },
+      /** 作者 */
+      author: {
+        text: 'JIM STUDIO',
+        href: '/work/expmale'
+      }
+    }
+  ];
   return (
     <div className="ScenicList-Component-Wrapper">
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Card.Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      {data.map((work) => (
+        <Card
+          key={work.name}
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt={work.name}
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Card.Meta title={work.name} description={work.name} />
+        </Card>
+      ))}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Home from './Home/home';
+import Head from 'next/head';
 import '../static/styles/index.scss';
 
 const Index = () => {
   return (
     <div>
-      <Home></Home>
+      <Head>
+        <title>景观设计</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Home/>
       {/* hello nextjs */}
     </div>
   );
